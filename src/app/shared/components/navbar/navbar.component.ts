076e4ad175c6649c5d1f1833cad363c5ab4ebdbc
Samuel Okeke <samwizzy11@gmail.com>
2022-05-17 14:37:42 +0100

    made homepage mobile responsive

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  showMobile: boolean = true;
+  showMobile: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
