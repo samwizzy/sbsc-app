67bd78c09a25cbe035926c1e219f5d515545366e
Samuel Okeke <samwizzy11@gmail.com>
2023-04-27 12:17:11 +0100

    commented gtag script in index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+
+declare var gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sbsc-app';
+
+  constructor(router: Router) {
+    // router.events
+    //   .pipe(filter((event: any) => event instanceof NavigationEnd))
+    //   .subscribe((event: NavigationEnd) => {
+    //     gtag('config', 'G-EXC72BX6RP', {
+    //       path_path: event.urlAfterRedirects,
+    //     });
+    //   });
+  }
 }
--- src/app/modules/thank-you/thank-you.component.ts
+++ src/app/modules/thank-you/thank-you.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var dataLayer: any[];
+
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
-  styleUrls: ['./thank-you.component.scss']
+  styleUrls: ['./thank-you.component.scss'],
 })
 export class ThankYouComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // dataLayer.push({
+    //   currency: 'NGN',
+    //   amount: 25000,
+    // });
   }
-
 }
