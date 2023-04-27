fc9f84b2fbb3305ae93ba0a1f9a284033e34c439
Samuel Okeke <samwizzy11@gmail.com>
2023-04-27 06:42:47 +0100

    updated server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import 'zone.js/dist/zone-node';
+import 'localstorage-polyfill';
 
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
@@ -8,16 +9,39 @@ import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
+const domino = require('domino');
+const fs = require('fs');
+const template = fs
+  .readFileSync(join('dist/sbsc-app/browser', 'index.html'))
+  .toString();
+const win = domino.createWindow(template);
+win.Object = Object;
+win.Math = Math;
+
+global['window'] = win;
+global['document'] = win.document;
+global['Object'] = win.Object;
+global['HTMLElement'] = win.HTMLElement;
+global['navigator'] = win.navigator;
+global['localStorage'] = localStorage;
+global['Event'] = win.Event;
+global['Event']['prototype'] = win.Event.prototype;
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/sbsc-app/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  server.engine(
+    'html',
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -25,13 +49,19 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
   });
 
   return server;
@@ -52,7 +82,7 @@ function run(): void {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
