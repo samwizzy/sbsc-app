fd2c4b88a37889c8f35955cbdcb644a569111fdd
Samuel Okeke <samwizzy11@gmail.com>
2023-04-28 16:23:43 +0100

    added gtag to thank you page for testing

--- src/app/modules/thank-you/thank-you.component.ts
+++ src/app/modules/thank-you/thank-you.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
 import { SeoService } from 'src/app/core/services/seo.service';
-declare var gtag: Function;
 
 const meta = {
   title: 'SBSC | Thank You',
@@ -15,16 +12,8 @@ const meta = {
   styleUrls: ['./thank-you.component.scss'],
 })
 export class ThankYouComponent implements OnInit {
-  constructor(router: Router, private seo: SeoService) {
+  constructor(private seo: SeoService) {
     this.seo.setMetaTags(meta);
-
-    router.events
-      .pipe(filter((event: any) => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        gtag('config', 'G-EXC72BX6RP', {
-          path_path: event.urlAfterRedirects,
-        });
-      });
   }
 
   ngOnInit(): void {}
