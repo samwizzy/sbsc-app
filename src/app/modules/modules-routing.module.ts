a301d0001ccfe77bac9f8ee0df41c57080ef0704
Samuel Okeke <samwizzy11@gmail.com>
2023-05-12 06:15:24 +0100

    add meta description 2

--- src/app/modules/modules-routing.module.ts
+++ src/app/modules/modules-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ParantComponent } from '../parant/parant.component';
-import { LongformComponent } from './longform/longform.component';
-import { ActionsComponent } from './actions/actions.component';
-import { ThankYouComponent } from './thank-you/thank-you.component';
+// import { ParantComponent } from '../parant/parant.component';
+// import { LongformComponent } from './longform/longform.component';
+// import { ActionsComponent } from './actions/actions.component';
+// import { ThankYouComponent } from './thank-you/thank-you.component';
 import { ProtectedGuard } from '../core/guards/protected.guard';
 import { HomeComponent } from './home/home.component';
 import { ModulesComponent } from './modules.component';
@@ -20,10 +20,10 @@ const routes: Routes = [
         loadChildren: () =>
           import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
       },
-      { path: 'parant', component: ParantComponent },
-      { path: 'longform', component: LongformComponent },
-      { path: 'actions', component: ActionsComponent },
-      { path: 'thankyou', component: ThankYouComponent },
+      // { path: 'parant', component: ParantComponent },
+      // { path: 'longform', component: LongformComponent },
+      // { path: 'actions', component: ActionsComponent },
+      // { path: 'thankyou', component: ThankYouComponent },
     ],
   },
 ];
